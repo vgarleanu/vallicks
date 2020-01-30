84c41620a5e58c780fc1489692a78e8243a993c8
Valerian Garleanu <valerian.garleanu@protonmail.com>
2020-01-30 23:22:44 +0000

    added support for port_base enumeration

--- src/pci.rs
+++ src/pci.rs
@@ -23,6 +23,22 @@ pub struct Device {
     interface_id: u8,
     revision: u8,
     interrupt: u16,
+
+    port_base: Option<u32>,
+}
+
+#[derive(Debug)]
+pub struct BaseAddrReg {
+    addr: u32,
+    size: u32,
+    reg_type: DeviceType,
+    prefetch: bool,
+}
+
+#[derive(Debug)]
+pub enum DeviceType {
+    MemoryMapping = 0,
+    InputOutput = 1,
 }
 
 impl Pci {
@@ -47,6 +63,7 @@ impl Pci {
 
             revision: self.read(bus, device, function, 0x08) as u8,
             interrupt: self.read(bus, device, function, 0x3c) & 0x00ff,
+            port_base: None,
         }
     }
 
@@ -55,9 +72,21 @@ impl Pci {
         for bus in 0..8 {
             for dev in 0..32 {
                 for fnt in 0..8 {
-                    let device = self.get_device(bus, dev, fnt);
+                    let mut device = self.get_device(bus, dev, fnt);
                     if device.vendor_id <= 0x0004 || device.vendor_id == 0xffff {
                         continue;
+                    }
+
+                    for i in 0..6 {
+                        let bar = self.get_base_addr_reg(bus, dev, fnt, i);
+                        if let Some(x) = bar {
+                            match x.reg_type {
+                                DeviceType::InputOutput => {
+                                    device.port_base = Some(x.addr as u32);
+                                }
+                                _ => {}
+                            }
+                        }
                     }
 
                     devices.push(device);
@@ -66,14 +95,41 @@ impl Pci {
         }
 
         devices.sort_by(|a, b| a.device_id.cmp(&b.device_id));
-        devices.dedup_by(|b, a| a.device_id.eq(&b.device_id));
 
         for d in devices {
             sprintln!("{:x?}", d);
         }
     }
 
-    fn get_base_addr_reg(&mut self, bus: u16, device: u16, fun: u16, bar: u16) {}
+    fn get_base_addr_reg(
+        &mut self,
+        bus: u16,
+        device: u16,
+        fun: u16,
+        bar: u16,
+    ) -> Option<BaseAddrReg> {
+        let hdr_type = self.read(bus, device, fun, 0x0e) & 0x7f;
+        if bar >= 6 - (4 * hdr_type) {
+            return None;
+        }
+
+        let bar_val = self.read32(bus, device, fun, (0x10 + 4 * bar).into());
+        let dev_type = if (bar_val & 0x1) == 1 {
+            DeviceType::InputOutput
+        } else {
+            DeviceType::MemoryMapping
+        };
+
+        match dev_type {
+            DeviceType::InputOutput => Some(BaseAddrReg {
+                addr: (bar_val & 0xfffc) as u32,
+                size: 0,
+                reg_type: dev_type,
+                prefetch: false,
+            }),
+            _ => None,
+        }
+    }
 
     fn read(&mut self, bus: u16, device: u16, fun: u16, offset: u32) -> u16 {
         let id: u32 = 0x1 << 31
@@ -85,5 +141,17 @@ impl Pci {
         }
 
         unsafe { (self.data_port.read() >> (8 * (offset & 2)) & 0xffff) as u16 }
+    }
+
+    fn read32(&mut self, bus: u16, device: u16, fun: u16, offset: u32) -> u32 {
+        let id: u32 = 0x1 << 31
+            | ((bus as u32) << 16 | (device as u32) << 11 | (fun as u32) << 8) as u32
+            | (offset & 0xfc);
+
+        unsafe {
+            self.command_port.write(id);
+        }
+
+        unsafe { self.data_port.read() }
     }
 }
