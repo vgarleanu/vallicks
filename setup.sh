b4fcd3b85fd6abd2115cd5969f924bd84c9183e5
Valerian G <valerian.garleanu@pm.me>
2020-12-31 16:53:11 +0000

    Add script that adds a network bridge for qemu

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ip link add br0 type bridge
+ip addr flush dev br0
+ip addr add 192.168.100.50/24 brd 192.168.100.255 dev br0
+ip tuntap add mode tap user $(whoami)
+ip tuntap show
+ip link set tap0 master br0
+ip link set dev br0 up
+ip link set dev tap0 up
+dnsmasq --interface=br0 --bind-interfaces --dhcp-range=192.168.100.50,192.168.100.254
